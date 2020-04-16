b9d5c7da5d41a67da1437b8db43457bba2f475b5
Kailaria <kailaria86@gmail.com>
2020-04-15 19:34:06 -0500

    [Magic Square] - Adds new bingo logic file
    * Adds new bingo logic file for the more granular
    difficulty scale & magic square generation.

---
+++ bingoLogicMagicSquare.js
@@ -0,0 +1,6 @@
+/**
+ * Borrowed largely from BingoSync 
+ * (https://github.com/kbuzsaki/bingosync/blob/master/bingosync-app/generators/generator_bases/srl_generator_v5.js)
+ * (https://github.com/kbuzsaki/bingosync/blob/master/bingosync-app/generators/generator_bases/srl_generator_v8.js)
+ */
+
