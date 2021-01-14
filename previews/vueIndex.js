626eee5ed41291d44d22d28478881e2b59af7881
Kailaria <kailaria86@gmail.com>
2021-01-13 19:44:26 -0600

    [VueRefactor] Adds interactivity
    * Adds Update Log
    * Adds Change Log within Update Log
    * Adds interactive button to show/hide details about a change.
    * Adds a CSS file for the new index.

--- previews/vueIndex.js
+++ previews/vueIndex.js
@@ -1,6 +1,37 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        h1Title: 'This is a tooltip!',
+        description: 'This is a test project',
+        pvalue: 0,
+        updateLog: [
+            {
+                title: 'Beginning of the Vue refactor',
+                version: '1.02.01',
+                date: '2020-11-22',
+                summary: 'This is a test summary',
+                changeLog: [
+                    {
+                        id: '001',
+                        summary: 'Change #1',
+                        isDisplayDetails: false,
+                    },
+                    {
+                        id: '002',
+                        summary: 'Change #2',
+                        isDisplayDetails: false,
+                    },
+                ]
+            }
+        ],
+    },
+    methods: {
+        toggleChangeDetails: function(updateVersion, changeId) {
+            var theChange = this.updateLog.find(update => update.version === updateVersion)
+                .changeLog.find(change => change.id === changeId);
+
+            theChange.isDisplayDetails = !theChange.isDisplayDetails;
+        }
     }
 })
