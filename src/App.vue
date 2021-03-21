e68e6c05c7881d44f068772d29f30d212831b556
Kailaria <kailaria86@gmail.com>
2021-03-21 04:25:21 -0500

    [VueRefactor] Follow Vue naming style guide

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 // import HelloWorld from './components/HelloWorld.vue';
-import VersionLogView from './views/VersionLogView.vue';
+import VersionLogView from './views/VersionLog.vue';
 
 @Options({
   components: {
