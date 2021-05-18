77ec72b42c1fb4d6740e2b0d21f5aa267c360958
Kailaria <kailaria86@gmail.com>
2021-05-18 15:16:03 -0500

    [VueRefactor] Mild style changes & data update * Left-align default * Center-align nav bar * Removed irrelevant copy-paste in Update 1.01.00 change 6

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,13 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
 }
 
 #nav {
   padding: 30px;
+  text-align: center;
 
   a {
     font-weight: bold;
