245c8c2843f6f676704d4bc31d226cb1cb0597e7
Kailaria <kailaria86@gmail.com>
2021-06-29 03:27:08 -0500

    [VueRefactor] Implements UpdateLog store

---
+++ src/store/modules/update-logs/update-log-module.ts
@@ -0,0 +1,40 @@
+import {Module, VuexModule, getModule, MutationAction} from "vuex-module-decorators"
+import store from '@/store/index'
+import axios from 'axios';
+import Update from '@/models/Update';
+import * as common from '@/static/UpdateLogCommon';
+
+@Module({
+    name: 'updateLog', 
+    store, 
+    dynamic: true
+})
+class UpdateLogsModule extends VuexModule {
+    updates: Array<Update> = new Array<Update>();
+    
+    get isLoaded(): boolean {
+        return this.updates.length >= common.updateFileNames.length;
+    }
+
+    get findByVersion() {
+        return (versionId: string) => this.updates.find(update => update.version === versionId);
+    }
+
+    @MutationAction
+    async initUpdateLog() {
+        const parser = require('fast-xml-parser');
+        const baseUrl = window.location.origin + '/updateLogs/Update_';
+        const xmlSuffix = '.xml';
+        let updateLog = new Array<Update>();
+        
+        for(let i = 0; i < common.updateFileNames.length; i++) {
+            let fileName = common.updateFileNames[i];
+            let update = await axios.get(baseUrl + fileName + xmlSuffix);
+            let jsonObj = parser.parse(update.data);
+            updateLog.push(Update.CopyFromJson(jsonObj.update));
+        }
+        return {updates: updateLog};
+    }
+}
+
+export default getModule(UpdateLogsModule);
--- src/views/UpdateLog.vue
+++ src/views/UpdateLog.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div v-if="!isUpdateLogEmptyOrNull" class="col-sm-3 list-group">
+            <div v-if="isUpdateLogLoaded" class="col-sm-3 list-group">
                 <a
                     class="list-group-item list-group-item-action update-sidebar-item" 
                     v-for="update in updateLog"
@@ -12,7 +12,7 @@
                     <div>{{ update.date }}</div>
                 </a>
             </div>
-            <!-- <update-log-sidebar :update-log="updateLog" @select-update="onSelectUpdate()"/> -->
+            <!-- <update-log-sidebar @select-update="onSelectUpdate()"/> -->
             <div v-if="displayedUpdate !== undefined" class="col-sm-9" id="displayUpdateDiv">
                 <!-- style="flex:flex-grow;" id="displayUpdateDiv"> -->
                 <h2>{{ displayedUpdate.title }}</h2>
@@ -43,30 +43,21 @@
 
 <script lang="ts">
 import {Options, Vue} from 'vue-class-component';
-import axios from 'axios';
 import Update from '@/models/Update';
-import * as common from '@/static/UpdateLogCommon';
+import UpdateLogsModule from '@/store/modules/update-logs/update-log-module';
 
 @Options({})
 export default class UpdateLog extends Vue
 {
-    updateLog: Array<Update> = new Array<Update>();
     displayedUpdateVersion: string = "";
 
     constructor(obj: any) {
         super(obj);
-        this.updateLog = new Array<Update>();
+        UpdateLogsModule.initUpdateLog();
     }
 
     toggleChangeDetails(updateVersion: string, changeId: number) {
-        // This block is here solely to make the compiler happy that updateLog is valid.
-        if (this.updateLog === undefined || this.updateLog.length === 0) {
-            this.initUpdateLog();
-            if (this.updateLog === undefined || this.updateLog.length === 0) {
-                return;
-            }
-        }
-        let theUpdate = this.updateLog.find(update => update.version === updateVersion)
+        let theUpdate = UpdateLogsModule.updates.find(update => update.version === updateVersion)
         if (theUpdate === undefined)
         {
             return;
@@ -78,58 +69,40 @@ export default class UpdateLog extends Vue
         }
     }
     onSelectUpdate(updateVersion: string) {
-        if ((this.updateLog === undefined || this.updateLog.length === 0)) {
-            this.initUpdateLog();
-            if (this.updateLog === undefined || this.updateLog.length === 0) {
-                return;
-            }
-        }
         if (this.displayedUpdate !== undefined && this.displayedUpdate.changeLog.length > 0) {
             this.displayedUpdate.changeLog.every(change => change.isDisplayDetails = false);
         }
-        let foundUpdate = this.updateLog.find(update => update.version === updateVersion);
+        let foundUpdate = UpdateLogsModule.updates.find(update => update.version === updateVersion);
         this.displayedUpdateVersion = foundUpdate === undefined ? this.displayedUpdateVersion : updateVersion;
     }
 
     initUpdateLog() {
-        if (!this.isUpdateLogEmptyOrNull) {
+        if (this.isUpdateLogLoaded) {
             return;
         }
-        const parser = require('fast-xml-parser');
-        const baseUrl = window.location.origin + '/updateLogs/Update_';
-        const xmlSuffix = '.xml';
-        let _self = this;
-        // TODO: Make async...?
-        common.updateFileNames.forEach(fileName => {
-            axios(baseUrl + fileName + xmlSuffix).then(response => {
-                let jsonObj = parser.parse(response.data);
-                if (_self.updateLog !== undefined) {
-                    _self.updateLog.push(Update.CopyFromJson(jsonObj.update));
-                    if (_self.displayedUpdateVersion === undefined) {
-                        _self.displayedUpdateVersion = _self.updateLog[0].version;
-                    }
-                }
-            })
-            .catch(err => console.log(err))
-        });
+        UpdateLogsModule.initUpdateLog();
     }
 
-    get isUpdateLogEmptyOrNull() : boolean {
-        return this.updateLog === undefined || this.updateLog.length === 0;
+    get isUpdateLogLoaded() : boolean {
+        return UpdateLogsModule.isLoaded;
     }
 
     get displayedUpdate() : Update {
-        if ((this.updateLog === undefined || this.updateLog.length === 0)) {
-            this.initUpdateLog();
-            if (this.updateLog === undefined || this.updateLog.length === 0) {
-                return new Update();
-            }
+        if (!this.isUpdateLogLoaded) {
+            return new Update();
         }
         if (this.displayedUpdateVersion === '') {
-            this.displayedUpdateVersion = this.updateLog[0].version;
+            this.displayedUpdateVersion = UpdateLogsModule.updates[0].version;
         }
-        let theUpdate = this.updateLog.find(update => update.version === this.displayedUpdateVersion);
+        let theUpdate = UpdateLogsModule.findByVersion(this.displayedUpdateVersion);
         return theUpdate === undefined ? new Update() : theUpdate;
+    }
+
+    get updateLog() : Array<Update> {
+        if (!UpdateLogsModule.isLoaded) {
+            return new Array<Update>();
+        }
+        return UpdateLogsModule.updates;
     }
 }
 </script>
