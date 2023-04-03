56b1bdfae30a24c5a49fbf7329d497431dde9267
Kailaria <kailaria86@gmail.com>
2023-04-03 01:17:59 -0500

    [Gen New] Adds calculated variables * Current Seed from generator options * Expandable Options for the next task

--- src/components/BingoGeneratorUsage.vue
+++ src/components/BingoGeneratorUsage.vue
@@ -1,13 +1,13 @@
 <template>
     <ul>
         <li>Hover over a goal when you want an explanation about that goal.</li>
-        <li>Click on a goal to advance forward.</li>
-        <li>Shift-Click on a goal to advance backward.</li>
+        <li>Click on a goal to advance forward. Board goals loop from "Done" to "Failed", while the End Goal loops from "Done" to "Not Done".</li>
+        <li>Shift-Click on a goal to advance backward. Status loops to Done from the least done status.</li>
         <li>
             <span>Goal coloration and order:</span>
             <ul>
                 <li>Red: "Not going to complete" or "Failed challenge"</li>
-                <li>Gray: "Not Done"</li>
+                <li>Gray: "Not Done" (starting/default)</li>
                 <li>Yellow: "Planning to complete" or "In Progress"</li>
                 <li>Green: "Done!"</li>
             </ul>
--- src/components/GenerateNewBoard.vue
+++ src/components/GenerateNewBoard.vue
@@ -1,19 +1,26 @@
 <template>
     <div
-        id="generateNew-container">
+        id="generateNew-container"
+    >
         <label for="newSeed">Seed: </label>
         <input v-model="newSeed" type="text">
-        <button @click="updateTextSeed()"></button>
+        <button @click="updateTextSeed()">Generate</button>
+    </div>
+    <div>Current Seed: {{ currentSeed }}</div>
+    <div v-if="areOptionsExpanded">
+        options expanded :3
     </div>
 </template>
 
 <script lang="ts">
+// The main goal here is to just store, modify, and provide the generator options upward.
+// DO NOT generate the board in here, only emit the updated text seed.
 import {Options, Vue} from 'vue-class-component';
 import GeneratorOptions from '@/models/GeneratorOptions';
 
 @Options({
     props: {
-        expandedOptions: Boolean,
+        expandableOptions: Boolean,
     },
     emits: [
         'updateTextSeed'
@@ -23,7 +30,27 @@ export default class GenerateNewBoard extends Vue
 {
     newSeed!: string;
     generatorOptions!: GeneratorOptions;
+    expandableOptions!: boolean;
+    expandOptions!: boolean;
 
+    mounted() {
+        this.expandOptions = false;
+        this.generatorOptions = new GeneratorOptions('BoI', '1.1.1');
+    }
+
+    get areOptionsExpanded() : boolean {
+        return this.expandableOptions && this.expandOptions;
+    }
+
+    get currentSeed() : string {
+        let seed = 'loading';
+        if (this.generatorOptions !== undefined && this.generatorOptions.textSeed !== undefined) {
+            seed = this.generatorOptions.textSeed;
+        }
+        return seed;
+    }
+
+    // Emits the updated text seed so that the parent component can load the 
     updateTextSeed() {
         this.$emit('updateTextSeed', this.newSeed);
     }
